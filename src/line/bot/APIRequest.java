099cc4ea1ab4ba3c01da192fc291b9ce000b4b6d
Raj Sharma <rajconnect1612@gmail.com>
2017-09-21 16:59:05 +0900

    fix 4, refactor separate class

---
+++ src/line/bot/APIRequest.java
@@ -0,0 +1,80 @@
+package line.bot;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONObject;
+
+class APIRequest {
+
+	final String URL = "https://ads.line.me/api/v1.0";
+	final String Access_Token = "1djCb/mXV+KtryMxr6i1bXwJimZGZQho12gbGDoi5Gh/R9mwaw7NuppaB4ONTCf7NpnogdGbG4fvWJwTTMGT+nnysMoFTTDFJWlERwc7OU3EjVg0mB3NEEITwjCLPw8AGb7dghhNEY27vya6vq4b3I9PbdgDzCFqoOLOYbqAITQ=";
+	String object_type = "";
+	String request_type = "";
+	String params[] = null;
+
+	APIRequest(String object_type, String request_type) {
+		this.object_type = object_type;
+		this.request_type = request_type;
+	}
+	
+	APIRequest(String object_type, String request_type, String params[]) {
+		this.object_type = object_type;
+		this.request_type = request_type;
+		this.params = params;
+	}
+	
+	
+
+	public APIRequest(String string, String string2, String string3, String string4) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public JSONObject send_post() {
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		JSONObject jsonObject = null;
+
+		try {
+
+			HttpPost request = new HttpPost(this.URL + this.object_type + this.request_type);
+			StringEntity params = null;
+			if (this.request_type.equals("/get")) {
+				params = new StringEntity("{\"accountId\":\"5514\"}");
+			} else if (this.request_type.equals("/set")) {
+//				params = new StringEntity("{\"accountId\":\"5514\",\"operands\":[{\"id\":40151,\"name\":\"ddddd\"}]}");
+				String output = String.format("{\"accountId\":\"5514\",\"operands\":[{\"id\":%s,\"name\":\"%s\"}]}", this.params[0], this.params[1]);
+				params = new StringEntity(output);
+			}
+			request.addHeader("content-type", "application/json");
+			request.addHeader("Authorization", "Bearer " + this.Access_Token);
+			request.setEntity(params);
+
+			HttpResponse response = httpClient.execute(request);
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+
+			System.out.println(result.toString());
+			jsonObject = new JSONObject(result.toString());
+
+		} catch (Exception ex) {
+
+			ex.printStackTrace();
+
+		} finally {
+
+		}
+		return jsonObject;
+
+	}
+
+}
--- src/line/bot/Bot.java
+++ src/line/bot/Bot.java
@@ -1,14 +1,7 @@
 package line.bot;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,7 +15,9 @@ public class Bot implements Runnable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bot bot = new Bot();
-		bot.getCampaigns(40151);
+		int campaign_id = 40151;
+		bot.getCampaigns(campaign_id);
+		bot.setCampaign();
 
 	}
 
@@ -43,6 +38,19 @@ public class Bot implements Runnable {
 	}
 
 	public void setCampaign() {
+		JSONObject jsonObject = campaign_list.get(0);
+		try {
+			String campaign_name = (String) jsonObject.get("name");
+			System.out.println(campaign_name);
+			// campaign_name = campaign_name + "★";
+			String params[] = {"40151","raj-----"};
+			APIRequest api_request = new APIRequest("/campaigns", "/set",params);
+			api_request.send_post();
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -67,50 +75,3 @@ public class Bot implements Runnable {
 
 }
 
-class APIRequest {
-
-	final String URL = "https://ads.line.me/api/v1.0";
-	final String Access_Token = "1djCb/mXV+KtryMxr6i1bXwJimZGZQho12gbGDoi5Gh/R9mwaw7NuppaB4ONTCf7NpnogdGbG4fvWJwTTMGT+nnysMoFTTDFJWlERwc7OU3EjVg0mB3NEEITwjCLPw8AGb7dghhNEY27vya6vq4b3I9PbdgDzCFqoOLOYbqAITQ=";
-	String object_type = "";
-	String request_type = "";
-
-	APIRequest(String object_type, String request_type) {
-		this.object_type = object_type;
-		this.request_type = request_type;
-	}
-
-	public JSONObject send_post() {
-		HttpClient httpClient = HttpClientBuilder.create().build();
-		JSONObject jsonObject = null;
-
-		try {
-
-			HttpPost request = new HttpPost(this.URL + this.object_type + this.request_type);
-			StringEntity params = new StringEntity("{\"accountId\":\"5514\"} ");
-			request.addHeader("content-type", "application/json");
-			request.addHeader("Authorization", "Bearer " + this.Access_Token);
-			request.setEntity(params);
-			HttpResponse response = httpClient.execute(request);
-
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			StringBuffer result = new StringBuffer();
-			String line = "";
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-
-			System.out.println(result.toString());
-			jsonObject = new JSONObject(result.toString());
-
-		} catch (Exception ex) {
-
-			ex.printStackTrace();
-
-		} finally {
-
-		}
-		return jsonObject;
-
-	}
-
-}
