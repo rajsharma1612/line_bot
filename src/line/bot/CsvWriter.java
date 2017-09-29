f12b28e8453975c77d6aad2a6073451216ffe818
Raj Sharma <rajconenct1612@gmail.com>
2017-09-29 13:27:17 +0900

    Implemented Write to CSV class.

--- src/line/bot/CsvWriter.java
+++ src/line/bot/CsvWriter.java
@@ -4,26 +4,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CsvWriter {
-	
-	static ArrayList<String> data = new ArrayList<String>();
-	
+
+	static String file_path = "Logs.csv";
+
 	public static void write_to_csv() {
-		data.add(Constants.ad_Account_Id);
-		data.add(String.valueOf(Constants.campaign_id));
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date date = new Date();
-		data.add(dateFormat.format(date));
-		
+
 		try {
-			FileOutputStream fileOutputStream = new FileOutputStream(new File("Logs.csv"));
-			String data_to_write = data.get(0)+";"+data.get(1)+";"+data.get(2);
+
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(file_path), true);
+			List<String> lines = Files.readAllLines(Paths.get(file_path), Charset.forName("UTF-8"));
+			if (lines.size() < 1) {
+				String data = "Ad Account ID,CPID,Date" + "\n";
+				fileOutputStream.write(data.getBytes());
+			}
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date date = new Date();
+
+			String data_to_write = Constants.ad_Account_Id + "," + Constants.CPID + "," + dateFormat.format(date)
+					+ "\n";
 			fileOutputStream.write(data_to_write.getBytes());
+			fileOutputStream.close();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
